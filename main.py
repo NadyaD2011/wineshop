846a7bd49f94d34528d14e82d46c2f1fe9c6e907
NadyaEvp <t_evp@mail.ru>
2024-08-13 20:11:29 +0300

    Оберните код в ifmain / def main

--- main.py
+++ main.py
@@ -4,37 +4,65 @@ import text_file_parser
 import argparse
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def get_file():
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(
+            ["html", "xml"],
+        ),
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template("template.html")
 
-
-date_now = datetime.datetime.now()
-date_of_foundation = datetime.datetime(
-    year=1921,
-    month=1,
-    day=1,
-    hour=0,
-)
-delta = date_now - date_of_foundation
-years = round(delta.days/365.2425)
-
-parser = argparse.ArgumentParser(
-    description='Insert path to text file here'
- )
-parser.add_argument('-f', '--file', help='Insert path to text file here')
-args = parser.parse_args()
-file_name = args.file
-drinks = text_file_parser.fetch_drinks(file_name)
+    return template
 
 
-rendered_page = template.render(
-    years=years,
-    drinks=drinks,
-)
+def get_number_years_winery():
+    delta = datetime.datetime.now() - datetime.datetime(
+        year=1921,
+        month=1,
+        day=1,
+        hour=0,
+    )
+    years = round(delta.days / 365.2425)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    return years
+
+
+def fill_file(
+    template,
+    years,
+    drinks,
+):
+    rendered_page = template.render(
+        years=years,
+        drinks=drinks,
+    )
+
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Введите здесь путь к текстовому файлу",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Введите здесь путь к текстовому файлу",
+    )
+    args = parser.parse_args()
+    file_name = args.file
+    drinks = text_file_parser.fetch_drinks(file_name)
+    template = get_file()
+    years = get_number_years_winery()
+    fill_file(
+        template,
+        years,
+        drinks,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- text_file_parser.py
+++ text_file_parser.py
@@ -3,16 +3,15 @@ import re
 
 def parse_item(item):
     try:
-        first_index = re.search('[:] ', item).end()
+        first_index = re.search("[:] ", item).end()
         return item[first_index:]
     except AttributeError:
         return None
 
 
 def separate_type_and_units(item):
-
-    drink_type_last_index = re.search('\n\n', item).end() - 2
-    if item.startswith('#'):
+    drink_type_last_index = re.search("\n\n", item).end() - 2
+    if item.startswith("#"):
         first_index = 2
         drink_type = item[first_index:drink_type_last_index]
     else:
@@ -22,29 +21,29 @@ def separate_type_and_units(item):
 
 
 def parse_drinks(items):
-    specifications = items.split('\n')
-    is_profitable = 'Выгодное предложение' in specifications # mindblow
+    specifications = items.split("\n")
+    is_profitable = "Выгодное предложение" in specifications
     for specification in specifications:
-        if specification.startswith('Название:'):
+        if specification.startswith("Название:"):
             name = parse_item(specification)
-        elif specification.startswith('Сорт:'):
+        elif specification.startswith("Сорт:"):
             variety = parse_item(specification)
-        elif specification.startswith('Цена:'):
+        elif specification.startswith("Цена:"):
             price = parse_item(specification)
-        elif specification.startswith('Картинка:'):
+        elif specification.startswith("Картинка:"):
             picture = parse_item(specification)
     drink_specs = {
-        'name': name,
-        'variety': variety,
-        'price': price,
-        'picture': picture,
-        'is_profitable': is_profitable,
+        "name": name,
+        "variety": variety,
+        "price": price,
+        "picture": picture,
+        "is_profitable": is_profitable,
     }
     return drink_specs
 
 
 def read_text_file(file_name):
-    with open(file_name, 'r', encoding='UTF-8') as file:
+    with open(file_name, "r", encoding="UTF-8") as file:
         text = file.read()
     return text
 
@@ -52,15 +51,19 @@ def read_text_file(file_name):
 def parse_text(text):
 
     drinks = []
-    split_text = text.split('\n\n\n# ')
+    split_text = text.split("\n\n\n# ")
     for drinks_by_type in split_text:
-        drinks_type_name, drinks_by_type_units = separate_type_and_units(drinks_by_type)
-        split_drinks_by_type_units = drinks_by_type_units.split('\n\n')
-        drink_units = [parse_drinks(drink_unit) for drink_unit in split_drinks_by_type_units]
+        drinks_type_name, drinks_by_type_units = separate_type_and_units(
+            drinks_by_type,
+        )
+        split_drinks_by_type_units = drinks_by_type_units.split("\n\n")
+        drink_units = [
+            parse_drinks(drink_unit) for drink_unit in split_drinks_by_type_units
+        ]
 
         one_type_drinks = {
-            'type': drinks_type_name,
-            'units': drink_units,
+            "type": drinks_type_name,
+            "units": drink_units,
         }
         drinks.append(one_type_drinks)
     return drinks
